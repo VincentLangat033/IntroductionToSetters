95f000d4102e95fbe328a6bd0572480e36c0806f
Vinnie Kim <>
2022-08-24 08:47:02 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,28 @@
+# puts "Hello, World!"
+class Student
+  def initialize(name, course)
+    @name= name,
+    @course = course
+  end
+  # A getter method for Name
+  def name
+    @name
+  end
+  # A getter method for course
+  def course
+    @course
+  end
+  # A setter Method
+  def set_course=course
+    @course=course
+  end
+    def course=course
+    @course=course
+  end
+
+  
+end
+student=Student.new("Vincent Kimutai", "Software Enginer")
+puts student.set_course = "Data Engineer"
+puts student.course = "Data Engineer Two"
+puts student.name
